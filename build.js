4cc7a3da4e0e4b1301a46b8aa53e78cd11c460fa
JPaulus04 <joshua.r.paulus@gmail.com>
2026-09-16 22:37:52 -0400

    Add files via upload

--- build.js
+++ build.js
@@ -202,7 +202,7 @@ html = html.replace('<script src="src/centralHQPrototype.js"></script>', '');
 
 requireMatch(!html.includes('<script src="src/main.js"></script>'), 'game scripts were not bundled');
 requireMatch(!html.includes('<script src="src/centralHQPrototype.js"></script>'), 'prototype script was not bundled');
-requireMatch(html.includes('const LSC_BUILD = \'227\';'), 'Build 227 config is not present');
+requireMatch(html.includes('const LSC_BUILD = \'228\';'), 'Build 228 config is not present');
 requireMatch(html.includes('root.LSCWorldArt=Object.freeze') && html.indexOf('root.LSCWorldArt=Object.freeze') < html.indexOf('root.LSCReclamation=Object.freeze'), 'World art must be bundled before reclamation');
 requireMatch(html.includes('BALANCE.campaignPressure(run.phase)') && html.includes('campaignPressure: campaignPressure'), 'Campaign pressure integration missing');
 // WORLD and Command Base are fixed body-level siblings; prevent the Build 194 regression.
