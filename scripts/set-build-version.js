39a6a3c7edf2ffe20662447053f7b8c0e06f6cb8
JPaulus04 <joshua.r.paulus@gmail.com>
2026-09-23 21:51:13 -0400

    Add files via upload

--- scripts/set-build-version.js
+++ scripts/set-build-version.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const fs=require('fs');
 let c=fs.readFileSync('src/config.js','utf8');
-c=c.replace(/const LSC_BUILD = '\d+';/,"const LSC_BUILD = '235';").replace(/Build 23[0-9]/g,'Build 235');
+c=c.replace(/const LSC_BUILD = '\d+';/,"const LSC_BUILD = '236';").replace(/Build 23[0-9]/g,'Build 236');
 fs.writeFileSync('src/config.js',c,'utf8');
 let b=fs.readFileSync('build.js','utf8');
-b=b.replace(/Build 23[0-9]/g,'Build 235').replace(/LSC_BUILD = '23[0-9]'/g,"LSC_BUILD = '235'").replace(/Last Stand Command 23[0-9]/g,'Last Stand Command 235');
+b=b.replace(/Build 23[0-9]/g,'Build 236').replace(/LSC_BUILD = '23[0-9]'/g,"LSC_BUILD = '236'").replace(/Last Stand Command 23[0-9]/g,'Last Stand Command 236');
 fs.writeFileSync('build.js',b,'utf8');
-console.log('LSC build marker: 235');
+console.log('LSC build marker: 236');
